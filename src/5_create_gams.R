c1c1ad231025a68cb06a7bf6c63b174d5302eeae
Obliquitas <martin.miesbauer@gmail.com>
2024-08-29 10:42:01 +0200

    Plot gams of metrics to end time in individual files

--- src/5_create_gams.R
+++ src/5_create_gams.R
@@ -6,10 +6,23 @@ library(ggplot2)
 # Load the data
 ztc_emotion_metrics_end_times <- readRDS("data/ztc_emotion_metrics_end_times.rds")
 
-ztc_emotion_metrics_end_times %>%
-  gather("id", "value", 3:14) %>%
-  ggplot(aes(x = value, end)) +
-  geom_point() +
-  geom_smooth(method = "gam", formula = y ~ s(x, k = 5)) +
-  facet_wrap(~id, scales = "free") +
-  theme_minimal()
+# Gather the data into a long format
+long_data <- ztc_emotion_metrics_end_times %>%
+  gather("id", "value", 3:14)
+
+# Loop through each unique id and create a plot for each one
+unique_ids <- unique(long_data$id)
+
+for (id in unique_ids) {
+  # Filter data for the current id
+  plot_data <- long_data %>% filter(id == !!id)
+
+  p <- ggplot(plot_data, aes(x = value, y = end)) +
+    geom_point() +
+    geom_smooth(method = "gam", formula = y ~ s(x, k = 5)) +
+    ggtitle(id) +
+    theme_minimal()
+
+  # Save the plot
+  ggsave(filename = paste0("annotations_plot_", id, ".png"), plot = p)
+}
